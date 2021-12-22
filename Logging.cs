b76a0a6726e2549d5b316e9285dfc6fed81de273
Marat <farkhutdinovmr@gmail.com>
2021-12-22 13:03:21 +0500

    Task

--- Logging.cs
+++ Logging.cs
@@ -1,99 +1,40 @@
 ﻿using System;
 using System.IO;
-
+ 
 namespace Lesson
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Pathfinder fileLog = new Pathfinder(new FileLogWritter());
-            fileLog.Find("пишет лог в файл.");
-
-            Pathfinder consoleLog = new Pathfinder(new ConsoleLogWritter());
-            consoleLog.Find("пишет лог в консоль.");
-
-            Pathfinder fridayLogToFile = new Pathfinder(new SecureConsoleLogWritter(new FileLogWritter()));
-            fridayLogToFile.Find("пишет лог в файл по пятницам.");
-
-            Pathfinder fridayLogToConsole = new Pathfinder(new SecureConsoleLogWritter(new ConsoleLogWritter()));
-            fridayLogToConsole.Find("пишет лог в консоль по пятницам.");
-
-            Pathfinder consoleLogAndfridayToFile = new Pathfinder(ConsolePool.Create(new ConsoleLogWritter(), new SecureConsoleLogWritter(new FileLogWritter())));
-            consoleLogAndfridayToFile.Find("пишет лог в консоль а по пятницам ещэ и в файл.");
+ 
         }
     }
-
-    interface ILogger
+ 
+    class ConsoleLogWritter
     {
-        void WriteError(string message);
-    }
-
-    class Pathfinder
-    {
-        private ILogger _logger;
-
-        public Pathfinder(ILogger logger)
-        {
-            _logger = logger;
-        }
-
-        public void Find(string message)
-        {
-            _logger.WriteError(message);
-        }
-    }
-
-    class ConsolePool : ILogger
-    {
-        private ILogger[] _loggers;
-
-        public ConsolePool(ILogger[] loggers)
-        {
-            _loggers = loggers;
-        }
-
-        public void WriteError(string message)
-        {
-            foreach (ILogger logger in _loggers)
-                logger.WriteError(message);
-        }
-
-        public static ConsolePool Create(params ILogger[] loggers)
-        {
-            return new ConsolePool(loggers);
-        }
-    }
-
-    class ConsoleLogWritter : ILogger
-    {
-        public void WriteError(string message)
+        public virtual void WriteError(string message)
         {
             Console.WriteLine(message);
         }
     }
-
-    class FileLogWritter : ILogger
+ 
+    class FileLogWritter
     {
-        public void WriteError(string message)
+        public virtual void WriteError(string message)
         {
             File.WriteAllText("log.txt", message);
         }
     }
-
-    class SecureConsoleLogWritter : ILogger
+ 
+    class SecureConsoleLogWritter : ConsoleLogWritter
     {
-        ILogger _logger;
-
-        public SecureConsoleLogWritter(ILogger logger)
-        {
-            _logger = logger;
-        }
-
-        public void WriteError(string message)
+        public override void WriteError(string message)
         {
             if (DateTime.Now.DayOfWeek == DayOfWeek.Friday)
-                _logger.WriteError(message);
+            {
+                base.WriteError(message);
+            }
         }
     }
 }
